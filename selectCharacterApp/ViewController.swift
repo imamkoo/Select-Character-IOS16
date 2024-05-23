007b822da810a2e7814177f5baa0614f52f5435b
imamkoo <imamkholidubaidillah16@gmail.com>
2024-05-24 02:05:37 +0700

    first init

--- selectCharacterApp/ViewController.swift
+++ selectCharacterApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var charImage: UIImageView!
+    
+    var Char: UIImage = UIImage(imageLiteralResourceName: "Char")
+    var Char1: UIImage = UIImage(imageLiteralResourceName: "Char1")
+    var Char2: UIImage = UIImage(imageLiteralResourceName: "Char2")
+    var Char3: UIImage = UIImage(imageLiteralResourceName: "Char3")
+    var Char4: UIImage = UIImage(imageLiteralResourceName: "Char4")
+    var Char5: UIImage = UIImage(imageLiteralResourceName: "Char5")
+    
+    
+    @IBAction func selectButton(_ sender: UIButton) {
+        
+        let Char: [UIImage] = [Char, Char1, Char2, Char3, Char4,Char5]
+        
+        charImage.image = Char.randomElement()
     }
-
-
+    
 }
+
 
