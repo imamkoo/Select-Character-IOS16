bda3f1ee2e0ef23b46e982fd902e85d1c980fabf
imamkoo <imamkholidubaidillah16@gmail.com>
2024-05-23 23:37:18 +0700

    Initial Commit

---
+++ selectCharacterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  selectCharacterApp
+//
+//  Created by Mojokerto Lighting Project on 23/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
